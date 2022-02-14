b5f0b0119b0d04af87856f18b018dda15e630879
Turan <90398677+Turan141@users.noreply.github.com>
2022-02-14 12:48:02 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,93 +1,107 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react"
+import "./App.css"
 
-function App() {
-  const [isLoggedIn, setLogin] = useState(false);
-  const [email, setEmail] = useState("nobody");
 
-  function setEmailFn(newValue) {
-    setEmail(newValue);
-  }
-
-  const loginFn = () => {
-    setLogin({ isLoggedIn: true });
-  };
-
-  let activewindow;
-  isLoggedIn
-    ? (activewindow = <GreetingWindow email={email} />)
-    : (activewindow = (
-        <RegisterWindow onChange={setEmailFn} value={email} loginFn={loginFn} />
-      ));
-
-  return (
-    <>
-      <div className="appmain">{activewindow}</div>
-    </>
-  );
-}
 
 const GreetingWindow = ({ email }) => {
-  return (
-    <>
-      <div class="greetings">
-        <h1>Hello {email}</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="greetings">
+                <h1>Hello {email}</h1>
+            </div>
+        </>
+    );
 };
 
-function RegisterWindow({ onChange, value, loginFn }) {
-  let local = localStorage;
-
-  function loginAsAdmin(e) {
-    e.preventDefault();
-    let user = document.querySelector("#user");
-    for (let key in local) {
-      if (user.value == key) {
-        loginFn();
-        onChange(user.value);
-      }
+const RegisterWindow = ({ onChange, loginFn }) => {
+    let local = localStorage
+    let errorText = document.querySelector(".error")
+    const logPass = {
+        login: 'admin',
+        pass: 'admin'
     }
-  }
 
-  function createAdmin() {
-    localStorage.setItem("admin", "admin");
-  }
+    const handleSubmit = () => {
+        return loginAsAdmin()
+    }
 
-  function modal(e) {
-    let modal = document.querySelector(".showpass");
-    modal.innerHTML = "NOW YOU CAN ENTER WITH LOG:ADMIN PASS:ADMIN";
-  }
+    const loginAsAdmin = (e) => {
+        e.preventDefault()
+        let user = document.querySelector('#user')
+        let pass = document.querySelector('#pass')
+        for (let key in local) {
+            if (!user.value) {
+                return
+        }else if (user.value === logPass.login && pass.value === logPass.pass) {
+                loginFn()
+                onChange(user.value)
+                setLoggedIn()
 
-  return (
-    <>
-      <div id="wrapper">
-        <form id="signin" method="" action="" autocomplete="off">
-          <input type="text" id="user" name="user" placeholder="username" />
-          <input type="text" id="pass" name="pass" placeholder="password" />
-          <button onClick={loginAsAdmin} type="submit">
-            &#xf0da;
-          </button>
-          <p className="showpass">
-            dont have an account{" "}
-            <button
-              onClick={() => {
-                createAdmin();
-                modal();
-              }}
-              class="btn"
-            >
-              click to create
-            </button>
-          </p>
-        </form>
-      </div>
-      <div class="modal">
-        <h1>default log:admin pass:admin</h1>
-      </div>
-    </>
-  );
+            } else {
+                errorText.style.opacity = "100%"
+            }
+        }
+    }
+
+    const createAdmin = () => {
+        localStorage.setItem('admin', 'admin')
+    }
+
+    const setLoggedIn = () => {
+        return localStorage.setItem('online', 'true')
+    }
+
+    return (
+        <>
+            <div id="wrapper">
+                <h3 className={"error"}>Wrong pass</h3>
+                <form id="signin" method="" action="" autoComplete="off" onSubmit={handleSubmit}>
+                    <input type="text" id="user" name="user" placeholder="username" autoComplete="off" />
+                    <input type="password" id="pass" name="pass" placeholder="password" autoComplete="off" />
+                    <button onClick={loginAsAdmin} type="submit">
+                        &#xf0da;
+                    </button>
+                    <p className="showpass">
+                        dont have an account{" "}
+                        <button
+                            onClick={() => {
+                                createAdmin()
+                            }}
+                            className="btn"
+                        >
+                            click to create
+                        </button>
+                    </p>
+                </form>
+            </div>
+        </>
+    );
 }
 
+
+function App() {
+    const [isLoggedIn, setLogin] = useState(false);
+    const [email, setEmail] = useState('offline');
+
+    useEffect(() => {
+        setEmail(localStorage.getItem('admin'))
+    }, [])
+
+    const setEmailFn = (newValue) => {
+        setEmail(newValue)
+    }
+
+    const loginFn = () => {
+        setLogin({ isLoggedIn: true })
+    }
+
+    let isOnline = localStorage.getItem('online')
+
+    return (
+        <>
+            <div className="appmain">{isOnline ? <GreetingWindow email={email} /> : <RegisterWindow
+                onChange={setEmailFn} value={email} loginFn={loginFn} />}</div>
+        </>
+    )
+}
 export default App;
