ee312a7aab14b5c2346c92c520b43237806239f9
Turan <90398677+Turan141@users.noreply.github.com>
2022-02-14 23:11:57 +0400

    5th build

--- src/App.js
+++ src/App.js
@@ -1,107 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
 
-
-
-const GreetingWindow = ({ email }) => {
-    return (
-        <>
-            <div className="greetings">
-                <h1>Hello {email}</h1>
-            </div>
-        </>
-    );
-};
-
-const RegisterWindow = ({ onChange, loginFn }) => {
-    let local = localStorage
-    let errorText = document.querySelector('.error')
-    const logPass = {
-        login: 'admin',
-        pass: 'admin'
-    }
-
-    const handleSubmit = () => {
-        return loginAsAdmin()
-    }
-
-    const loginAsAdmin = (e) => {
-        e.preventDefault()
-        let user = document.querySelector('#user')
-        let pass = document.querySelector('#pass')
-        for (let key in local) {
-            if (!user.value) {
-                return
-        }else if (user.value === logPass.login && pass.value === logPass.pass) {
-                loginFn()
-                onChange(user.value)
-                setLoggedIn()
-
-            } else {
-                errorText.style.opacity = '100%'
-            }
-        }
-    }
-
-    const createAdmin = () => {
-        localStorage.setItem('admin', 'admin')
-    }
-
-    const setLoggedIn = () => {
-        return localStorage.setItem('online', 'true')
-    }
-
-    return (
-        <>
-            <div id="wrapper">
-                <h3 className={"error"}>Wrong pass</h3>
-                <form id="signin" method="" action="" autoComplete="off" onSubmit={handleSubmit}>
-                    <input type="text" id="user" name="user" placeholder="username" autoComplete="off" />
-                    <input type="password" id="pass" name="pass" placeholder="password" autoComplete="off" />
-                    <button onClick={loginAsAdmin} type="submit">
-                        &#xf0da;
-                    </button>
-                    <p className="showpass">
-                        dont have an account{" "}
-                        <button
-                            onClick={() => {
-                                createAdmin()
-                            }}
-                            className="btn"
-                        >
-                            click to create
-                        </button>
-                    </p>
-                </form>
-            </div>
-        </>
-    );
+const GreetingWindow = ({ userName }) => {
+  const logOutFn = () => {
+    localStorage.removeItem('admin');
+    return window.location.reload();
+  }
+  return (
+    <>
+      <div className='greetings'>
+        <h1>Hello {userName}</h1>
+        <button onClick={logOutFn}>LogOut</button>
+      </div>
+    </>
+  )
 }
 
+const RegisterWindow = () => {
+  const ERRORTEXT = document.querySelector('.error')
+
+  const LOGIN = 'admin'
+  const PASSWORD = 12345
+  const inputLoginRef = useRef()
+  const inputPasswordRef = useRef()
+  const loginAsAdmin = (e) => {
+    e.preventDefault()
+    console.log(inputLoginRef)
+    if (!inputLoginRef.current.value || !inputPasswordRef.current.value) {
+      return console.log('empty field')
+    } else if (
+      inputLoginRef.current.value === LOGIN &&
+      Number(inputPasswordRef.current.value) === PASSWORD
+    ) {
+      onSuccess()
+    } else {
+      ERRORTEXT.style.opacity = '100%';
+    }
+  };
+  const onSuccess = () => {
+    localStorage.setItem('admin', 'admin')
+    window.location.reload()
+  };
+
+  return (
+    <>
+      <div id="wrapper">
+        <h3 className={"error"}>Wrong pass</h3>
+        <form
+          id="signin"
+          method=""
+          action=""
+          autoComplete="off"
+          onSubmit={loginAsAdmin}
+        >
+          <input
+            type="text"
+            id="user"
+            name="user"
+            placeholder="username"
+            autoComplete="off"
+            ref={inputLoginRef}
+          />
+          <input
+            type="password"
+            id="pass"
+            name="pass"
+            placeholder="password"
+            autoComplete="off"
+            ref={inputPasswordRef}
+          />
+          <button type="submit">&#xf0da;</button>
+        </form>
+      </div>
+    </>
+  )
+}
 
 function App() {
-    const [isLoggedIn, setLogin] = useState(false);
-    const [email, setEmail] = useState('offline');
+  const [userName, setUsername] = useState(localStorage.getItem('admin'));
 
-    useEffect(() => {
-        setEmail(localStorage.getItem('admin'))
-    }, [])
+  useEffect(() => {
+    setUsername(localStorage.getItem('admin'));
+  }, [])
 
-    const setEmailFn = (newValue) => {
-        setEmail(newValue)
-    }
-
-    const loginFn = () => {
-        setLogin({ isLoggedIn: true })
-    }
-
-    let isOnline = localStorage.getItem('online')
-
-    return (
-        <>
-            <div className="appmain">{isOnline ? <GreetingWindow email={email} /> : <RegisterWindow
-                onChange={setEmailFn} value={email} loginFn={loginFn} />}</div>
-        </>
-    )
+  return (
+    <>
+      <div className='appmain'>
+        {Boolean(userName) ? (
+          <GreetingWindow userName={userName} />
+        ) : (
+          <RegisterWindow value={userName} />
+        )}
+      </div>
+    </>
+  )
 }
 export default App;
