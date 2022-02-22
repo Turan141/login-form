a859ff1c013a28df0645f28178d10f0857d55bd7
Turan <90398677+Turan141@users.noreply.github.com>
2022-02-22 13:29:27 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,19 +100,18 @@ function App() {
             }
         ];
 
-    function updateJson(userJSON){
-        let inputs = document.getElementsByTagName('input')
+    function updateJson(){
+        let inputs = document.querySelectorAll(".input")
+        console.log(inputs)
         for(let i = 0; i < inputs.length; i++){
             let userName = inputs[i].defaultValue
             for(let j = 0; j < i; j++){
                 if(userName === userJSON[j].username){
                      userJSON[j].username = inputs[i].value
-                    setEditMode(false)
-                    return
                 }
-            }
-            inputs[i].parentNode.innerHTML = `<p>${inputs[i].value}</p>`
+                inputs.forEach((elem) =>{ elem.parentNode.innerHTML = `<p>${elem.value}</p>`} )}
         }
+
     }
     function makeInput(e) {
         e.target.disabled = true
@@ -122,9 +121,9 @@ function App() {
            e.classList.add('show')
         })
         e.target.parentNode.parentNode.childNodes[0].innerHTML =
-            `<input value=${e.target.parentNode.parentNode.childNodes[0].innerHTML}>`
+            `<input class="input" value=${e.target.parentNode.parentNode.childNodes[0].innerHTML}>`
         e.target.parentNode.parentNode.childNodes[1].innerHTML =
-            `<input value=${e.target.parentNode.parentNode.childNodes[1].innerHTML}>`
+            `<input class="input" value=${e.target.parentNode.parentNode.childNodes[1].innerHTML}>`
     }
 
     let isLoggedIn = Boolean(userName)
@@ -136,7 +135,6 @@ function App() {
                 <td>{elem.username}</td>
                 <td className="idInput">{elem.id}</td>
                 {isLoggedIn ?<td><button disabled={false} onClick={makeInput} className="editBtn">Edit</button> </td> : null}
-                {isEditMode ? <td><button onClick={()=>updateJson(userJSON)} className="saveEditBtn">Save</button></td> : null}
             </tr>)
      })
     const userListTable = <div className="userListDiv">
@@ -146,6 +144,8 @@ function App() {
                     <td >Full Name</td>
                     <td>Username</td>
                     <td>ID</td>
+                    {isEditMode ? <td><button disabled={false} onClick={updateJson} className="saveEditBtn">Save</button></td> : null}
+
                 </tr>
             </thead>
           <tbody>
