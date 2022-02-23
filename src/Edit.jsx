7cb34eeb180830052c13c037fc0f119a7ff77e1f
Your Name <you@example.com>
2022-02-23 14:00:37 +0400

    new version

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function Edit(){
+  const [ element, setElement ] = useState('текст')
+  const [ inputType, setType ] = useState('text')
+  const setInput = () =>{
+    setElement(<div/>)
+    setType('text')
+  }
+  const setText = ()=>{
+    setElement(<input />)
+    setType('input')
+  }
   return(
-    <div>salamnecesen</div>
+    <>
+      <div>{element}</div>
+      {inputType==='text' ?
+        <button onClick={setText}>Edit</button>
+        :
+        <button onClick={setInput}>Confirm</button>}
+    </>
   )
 }
