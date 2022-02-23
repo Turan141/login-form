1b027f0c93766ae9be70ce8fe738a87715296eb0
Your Name <you@example.com>
2022-02-23 13:06:46 +0400

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./App.scss";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Edit from "./Edit";
-import MainWindow from "./MainWindow"
+import React, { useState, useRef, useEffect } from 'react'
+import './App.scss'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Edit from './Edit'
+import MainWindow from './MainWindow'
+
 
 function App() {
-        return (
-            <>
-                <header className="navbarDiv">
+  return (
+    <>
+      <header className="navbarDiv">
 
-                </header>
-                <div className="navRoute">
-                    <Link className="navbarElem" to="/admin">
+      </header>
+      <div className="navRoute">
+        <Link className="navbarElem" to="/admin">
                         Admin
-                    </Link>
-                    <Link className="navbarElem" to="/edit">
+        </Link>
+        <Link className="navbarElem" to="/edit">
                         Edit
-                    </Link>                </div>
-                <Routes>
-                    <Route path="/edit" element={<Edit/>}/>
-                    <Route path="/Admin" element={<MainWindow/>}/>
-                </Routes>
-            </>
-        );
+        </Link>                </div>
+      <Routes>
+        <Route path="/edit" element={<Edit/>}/>
+        <Route path="/Admin" element={<MainWindow/>}/>
+      </Routes>
+    </>
+  )
 }
-export default App;
+export default App
