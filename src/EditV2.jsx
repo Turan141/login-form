544e196ab44f4126bbc156708c3f73cf13f00f8b
Your Name <you@example.com>
2022-03-02 17:31:28 +0400

    aded multiple changes to editV2 module

--- src/EditV2.jsx
+++ src/EditV2.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react'
 
 
-const Section = ({ setInput, userList }) => {
+const Section = () => {
   return(
     <>
       <Content/>
@@ -12,13 +12,38 @@ const Section = ({ setInput, userList }) => {
 }
 const Content = () => {
   const [ textContent, setContent ] = useState('User')
+  const [ isEdit, setEdit ] = useState(false)
+  const [ isDisabled, setDisabled ] = useState(false)
+  const UserNameShow = ({ textContent }) => {
+    return(
+      <div className='userDiv'>
+        <div>{textContent}</div>
+        <button onClick={function setterFn(){setEdit(true) }}>Edit</button>
+      </div>
+    )
+  }
+
+  const EditNameShow = ({ setEdit, setContent }) => {
+    const confirmName = () => {
+      let name = document.querySelector('.input')
+      setContent(name.value)
+      setEdit(false)
+    }
+    return(
+      <>
+        <input className="input"/><button onClick={confirmName}>Confirm</button>
+      </>
+    )
+  }
+
   return(
-    <div className='userDiv'>
-      <div>{textContent}</div>
-      <button onClick={()=>{setContent(prompt('Введите имя пользовтеля'))}}>Edit</button>
-    </div>
+    <>
+      {!isEdit ? <UserNameShow textContent={textContent}/> : <EditNameShow setContent={setContent} setEdit={setEdit}/>}
+    </>
   )
 }
+
+
 function EditV2(){
   return(
     <>
