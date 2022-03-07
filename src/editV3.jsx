2aa4031be64ec5cb19cb4692fda618aaed5de5df
Your Name <you@example.com>
2022-03-07 09:52:58 +0400

    added 1 parrent

--- src/editV3.jsx
+++ src/editV3.jsx
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react'
 
 
 const Content = ({ changeFn, textContent }) => {
-  return(
+  return (
     <>
       <div>{textContent}</div>
       <button onClick={changeFn}>Edit</button>
@@ -11,7 +11,6 @@ const Content = ({ changeFn, textContent }) => {
 }
 
 const Form = ({ changeFn, setContent }) => {
-
   let inputRef = useRef()
 
   const applyChangefn = () => {
@@ -19,93 +18,135 @@ const Form = ({ changeFn, setContent }) => {
     changeFn()
   }
 
-  return(
+  return (
     <>
-      <input ref={inputRef}/>
+      <input ref={inputRef} />
       <button onClick={applyChangefn}>Save</button>
     </>
   )
 }
 
-const Parrent1 = () => {
-  const [ isEditing, setEditing ] = useState(false)
-  const [ textContent, setContent ] = useState('User')
+// const Parrent1 = () => {
+//   const [ isEditing, setEditing ] = useState(false)
+//   const [ textContent, setContent ] = useState('Valkyrie')
 
+//   const setInput = () => {
+//     setEditing(!isEditing)
+//   }
+
+//   return (
+//     <>
+//       <div className="typeFieldMain">
+//         {isEditing ? (
+//           <Form
+//             changeFn={setInput}
+//             textContent={textContent}
+//             setContent={setContent}
+//           />
+//         ) : (
+//           <Content textContent={textContent} changeFn={setInput} />
+//         )}
+//       </div>
+//     </>
+//   )
+// }
+// const Parrent2 = () => {
+//   const [ isEditing, setEditing ] = useState(false)
+//   const [ textContent, setContent ] = useState('Knight')
+
+//   const setInput = () => {
+//     setEditing(!isEditing)
+//   }
+
+//   return (
+//     <>
+//       <div className="typeFieldMain">
+//         {isEditing ? (
+//           <Form
+//             changeFn={setInput}
+//             textContent={textContent}
+//             setContent={setContent}
+//           />
+//         ) : (
+//           <Content textContent={textContent} changeFn={setInput} />
+//         )}
+//       </div>
+//     </>
+//   )
+// }
+// const Parrent3 = () => {
+//   const [ isEditing, setEditing ] = useState(false)
+//   const [ textContent, setContent ] = useState('Witch')
+
+//   const setInput = () => {
+//     setEditing(!isEditing)
+//   }
+
+//   return (
+//     <>
+//       <div className="typeFieldMain">
+//         {isEditing ? (
+//           <Form
+//             changeFn={setInput}
+//             textContent={textContent}
+//             setContent={setContent}
+//           />
+//         ) : (
+//           <Content textContent={textContent} changeFn={setInput} />
+//         )}
+//       </div>
+//     </>
+//   )
+// }
+
+const Element=({ textContent, isEditing, setEditing, setContent })=>{
   const setInput = () => {
     setEditing(!isEditing)
   }
-
   return(
     <>
       <div className="typeFieldMain">
-        {isEditing ?
-          <Form changeFn={setInput}
+        {isEditing ? (
+          <Form
+            changeFn={setInput}
             textContent={textContent}
-            setContent={setContent}/>
-          :
-          <Content textContent={textContent}
-            changeFn={setInput}/>
-        }
+            setContent={setContent}
+          />
+        ) : (
+          <Content textContent={textContent} changeFn={setInput} />
+        )}
       </div>
     </>
   )
 }
-const Parrent2 = () => {
-  const [ isEditing, setEditing ] = useState(false)
-  const [ textContent, setContent ] = useState('User')
 
-  const setInput = () => {
-    setEditing(!isEditing)
-  }
+const Parrent = () => {
+  const [ textContent1, setContent1 ] = useState('Witch')
+  const [ textContent2, setContent2 ] = useState('Knight')
+  const [ textContent3, setContent3 ] = useState('Valkyirie')
+  const [ isEditing1, setEditing1 ] = useState(false)
+  const [ isEditing2, setEditing2 ] = useState(false)
+  const [ isEditing3, setEditing3 ] = useState(false)
 
-  return(
+
+  return (
     <>
-      <div className="typeFieldMain">
-        {isEditing ?
-          <Form changeFn={setInput}
-            textContent={textContent}
-            setContent={setContent}/>
-          :
-          <Content textContent={textContent}
-            changeFn={setInput}/>
-        }
-      </div>
-    </>
-  )
-}
-const Parrent3 = () => {
-  const [ isEditing, setEditing ] = useState(false)
-  const [ textContent, setContent ] = useState('User')
-
-  const setInput = () => {
-    setEditing(!isEditing)
-  }
-
-  return(
-    <>
-      <div className="typeFieldMain">
-        {isEditing ?
-          <Form changeFn={setInput}
-            textContent={textContent}
-            setContent={setContent}/>
-          :
-          <Content textContent={textContent}
-            changeFn={setInput}/>
-        }
-      </div>
+      <Element textContent={textContent1} setContent={setContent1} isEditing={isEditing1} setEditing={setEditing1}/>
+      <Element textContent={textContent2} setContent={setContent2} isEditing={isEditing2} setEditing={setEditing2}/>
+      <Element textContent={textContent3} setContent={setContent3} isEditing={isEditing3} setEditing={setEditing3}/>
     </>
   )
 }
 
-function EditV3(){
-  return(
+function EditV3() {
+  return (
     <>
-      <Parrent1/>
-      <Parrent2/>
-      <Parrent3/>
+      {/* <Parrent1 />
+      <Parrent2 />
+      <Parrent3 /> */}
+      <Parrent />
     </>
   )
 }
-
 
 export { EditV3 }
